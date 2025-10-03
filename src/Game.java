64bc7b4350203254b5b117328e28ec431481611f
JosiyahBlue <josiyahb@nycstudents.net>
2025-10-03 11:49:54 -0400

    Done

---
+++ src/Game.java
@@ -0,0 +1,11 @@
+public class Game {
+    public Game(){
+        levelOne = new Level();
+        levelTwo = new Level();
+        levelThree = new Level();
+    }
+    private Level levelOne;
+    private Level levelTwo;
+    private Level levelThree;
+    private Boolean bonus;
+}
